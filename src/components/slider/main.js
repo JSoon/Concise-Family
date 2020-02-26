dbca221854321fb258e5e8bbe11f5b1b1c8c9c73
Soon <serdeemail@gmail.com>
2020-02-26 15:21:56 +0800

    [concise-slider] 1. Add indicator config 2. Add prev&next config 3. Generate indicator and prev&next controller dynamically

--- src/components/slider/main.js
+++ src/components/slider/main.js
@@ -1,24 +1,35 @@
 import './main.sass'
+import 'classlist-polyfill'
 
 /**
  * @description Concise slider component
+ * Note: This is not a gallery plugin, so far there is no autoplay function, which is not one of the intended features.
  * 
- * @param {object}  params            config object
- * @param {string}  params.selector   id or class selector
- * @param {number}  params.gridNum    number of visible grids
- * @param {number}  params.step       step number of items for each slide, default to gridNum
+ * @param {object}  params                config object
+ * @param {string}  params.selector       id or class selector
+ * @param {number}  params.gridNum        number of visible grids
+ * @param {number}  params.step           step number of items for each slide, default to gridNum
+ * @param {boolean} params.indicator      whether to generate indicator, default to true
+ * @param {boolean} params.indicatorCtrl  whether the indicator could control slide, default to false
+ * @param {string}  params.indicatorType  indicator type, page: one item represent gridNum slider items, single: one item maps a single slider item, default to page
+ * @param {boolean} params.prevNext       whether to generate prev & next slide controllers, default to true
  */
 
-let pageNum // Total indicator page number
-let curPageIdx = 0 // Current indicator page number index
+let indicatorItemNum // Total indicator item number
+let curIndicatorItemIdx = 0 // Current indicator item number index
 let curVisibleStart, curVisibleEnd // Current visible items index range
+let reachStart, reachEnd // If reach the start or end after slide
 
 const slider = params => {
 
   const {
     selector,
     gridNum,
-    step = params.gridNum
+    step = params.gridNum,
+    indicator = true,
+    indicatorCtrl = false,
+    indicatorType = 'page',
+    prevNext = true
   } = params
 
   if (!selector) {
@@ -34,12 +45,24 @@ const slider = params => {
   const sliderListWrapper = sliderWrapper.querySelector('.slider-list-wrapper')
   const sliderList = sliderWrapper.querySelector('.slider-list')
   const sliderItems = sliderWrapper.querySelectorAll('.slider-item')
-  const sliderIndicator = sliderWrapper.querySelector('.slider-indicator')
-  const sliderPrev = sliderWrapper.querySelector('.slider-prev')
-  const sliderNext = sliderWrapper.querySelector('.slider-next')
+  const sliderIndicator = document.createElement('div')
+  sliderIndicator.classList.add('slider-indicator')
+  indicator && sliderWrapper.appendChild(sliderIndicator)
+  const sliderPrev = document.createElement('div')
+  sliderPrev.innerHTML = 'Prev'
+  sliderPrev.classList.add('slider-prev')
+  const sliderNext = document.createElement('div')
+  sliderNext.innerHTML = 'Next'
+  sliderNext.classList.add('slider-next')
+  prevNext & sliderWrapper.appendChild(sliderPrev)
+  prevNext & sliderWrapper.appendChild(sliderNext)
 
-  // Indicator page number
-  pageNum = Math.ceil(sliderItems.length / gridNum)
+  // Indicator item number
+  if (indicatorType === 'page') {
+    indicatorItemNum = Math.ceil(sliderItems.length / gridNum)
+  } else {
+    indicatorItemNum = sliderItems.length
+  }
 
   // Calculate all the widths dynamically
   const wrapperWidth = sliderListWrapper.clientWidth
@@ -63,9 +86,10 @@ const slider = params => {
   sliderList.style.display = 'block'
 
   // Generate indicator items based on gridNum
-  for (let i = 0; i < pageNum; i++) {
+  for (let i = 0; i < indicatorItemNum; i++) {
     const indicatorItem = document.createElement('div')
     indicatorItem.classList.add('slider-indicator-item')
+    indicatorItem.setAttribute('data-idx', i)
     sliderIndicator.appendChild(indicatorItem)
   }
   sliderIndicator.firstElementChild.classList.add('active')
@@ -81,9 +105,18 @@ const slider = params => {
   })
 
   sliderList.addEventListener('transitionend', e => {
+    reachStart = curVisibleStart === 0 ? true : false
+    reachEnd = curVisibleEnd === sliderItems.length - 1 ? true : false
+
+    // Prevent transition of children element from fired
+    if (e.target !== sliderList) {
+      return
+    }
+
     updateIndicator({
       sliderIndicator,
-      step
+      indicatorType,
+      gridNum,
     })
   })
 
@@ -111,6 +144,30 @@ const slider = params => {
     })
   }, false)
 
+  // Indicator
+  if (indicatorCtrl) {
+    sliderIndicator.children.forEach(item => item.style.cursor = 'pointer')
+    sliderIndicator.addEventListener('click', e => {
+      if (!e.target.hasAttribute('data-idx')) {
+        return
+      }
+
+      const newIndicatorItemIdx = Number(e.target.getAttribute('data-idx'))
+      console.log('newIndicatorItemIdx', newIndicatorItemIdx);
+
+      updateListPosition({
+        direction: 'locating',
+        sliderList,
+        sliderItems,
+        step,
+        gridNum,
+        itemWidth,
+        indicatorType,
+        newIndicatorItemIdx
+      })
+    }, false)
+  }
+
   //#endregion
 
 }
@@ -123,6 +180,8 @@ function updateListPosition(params) {
     step,
     gridNum,
     itemWidth,
+    indicatorType,
+    newIndicatorItemIdx
   } = params
 
   if (gridNum > sliderItems.length) {
@@ -135,8 +194,14 @@ function updateListPosition(params) {
     if (curVisibleStart < 0) {
       curVisibleStart = 0
     }
-  } else {
+  } else if (direction === 'next') {
     curVisibleStart += step
+  } else if (direction === 'locating') {
+    if (indicatorType === 'page') {
+      curVisibleStart = newIndicatorItemIdx * gridNum
+    } else {
+      curVisibleStart = newIndicatorItemIdx
+    }
   }
   curVisibleEnd = curVisibleStart + gridNum - 1
 
@@ -145,22 +210,16 @@ function updateListPosition(params) {
     curVisibleEnd = sliderItems.length - 1
     curVisibleStart = curVisibleEnd + 1 - gridNum
   }
-
-  // Calculate current page number index
-  if (direction === 'prev') {
-    curPageIdx = Math.floor(curVisibleStart / gridNum)
-  } else {
-    // If the number of visible items is not equal to gridNum
-    if ((curVisibleEnd + 1) % gridNum !== 0) {
-      // Then it's the last page
-      curPageIdx = pageNum - 1
-    } else {
-      curPageIdx = Math.floor(curVisibleStart / gridNum)
-    }
+  if (reachStart && direction === 'prev') {
+    curVisibleEnd = sliderItems.length - 1
+    curVisibleStart = curVisibleEnd + 1 - gridNum
+  }
+  if (reachEnd && direction === 'next') {
+    curVisibleStart = 0
+    curVisibleEnd = curVisibleStart + gridNum - 1
   }
 
-  console.log('curPageIdx', curPageIdx);
-  console.log('curVisibleRange', `[${curVisibleStart}, ${curVisibleEnd}]`);
+  console.log('curVisibleRange', [curVisibleStart, curVisibleEnd]);
 
   sliderList.style.transform = `translate3d(-${curVisibleStart * itemWidth}px, 0, 0)`
 
@@ -168,12 +227,29 @@ function updateListPosition(params) {
 
 function updateIndicator(params) {
   const {
-    sliderIndicator
+    sliderIndicator,
+    indicatorType,
+    gridNum
   } = params
+
+  // Calculate current page number index
+  if (indicatorType === 'page') {
+    if (reachEnd) {
+      curIndicatorItemIdx = indicatorItemNum - 1
+    } else {
+      if (curVisibleStart % gridNum === 0) {
+        curIndicatorItemIdx = curVisibleStart % gridNum
+      }
+    }
+  } else {
+    curIndicatorItemIdx = curVisibleStart
+  }
+
+  console.log('curIndicatorItemIdx', curIndicatorItemIdx);
 
   // Highlight current indicator based on step
   sliderIndicator.children.forEach(item => item.classList.remove('active'))
-  sliderIndicator.children[curPageIdx].classList.add('active')
+  sliderIndicator.children[curIndicatorItemIdx].classList.add('active')
 
 }
 
